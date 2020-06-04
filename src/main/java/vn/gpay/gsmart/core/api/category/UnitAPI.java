dc60f4e65455dc0d5edf159f0a8a1e6c664dc994
Ha Nguyen Vinh <vinhha2701@gmail.com>
2020-06-04 16:09:16 +0700

    Unit API update

--- src/main/java/vn/gpay/gsmart/core/api/category/UnitAPI.java
+++ src/main/java/vn/gpay/gsmart/core/api/category/UnitAPI.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import vn.gpay.gsmart.core.base.ResponseBase;
 import vn.gpay.gsmart.core.category.IUnitService;
+import vn.gpay.gsmart.core.category.Port;
+import vn.gpay.gsmart.core.category.Unit;
 import vn.gpay.gsmart.core.utils.ResponseMessage;
 
 
@@ -32,4 +35,54 @@ public class UnitAPI {
 		    return new ResponseEntity<Unit_getall_response>(response,HttpStatus.OK);
 		}
 	}
+	
+	@RequestMapping(value = "/getUnitById",method = RequestMethod.POST)
+	public ResponseEntity<GetUnitById_response> GetUnitByid(@RequestBody GetUnitById_request entity, HttpServletRequest request ) {//@RequestParam("id") 
+		GetUnitById_response response = new GetUnitById_response();
+		try {
+			response.data = unitservice.findOne(entity.id);
+			response.setRespcode(ResponseMessage.KEY_RC_SUCCESS);
+			response.setMessage(ResponseMessage.getMessage(ResponseMessage.KEY_RC_SUCCESS));
+			return new ResponseEntity<GetUnitById_response>(response,HttpStatus.OK);
+		}catch (Exception e) {
+			response.setRespcode(ResponseMessage.KEY_RC_SERVER_ERROR);
+			response.setMessage(e.getMessage());
+		    return new ResponseEntity<GetUnitById_response>(response,HttpStatus.OK);
+		}
+	}
+	
+	@RequestMapping(value = "/createUnit",method = RequestMethod.POST)
+	public ResponseEntity<ResponseBase> CreateUnit(@RequestBody Unit_create_request entity, HttpServletRequest request ) {//@RequestParam("type") 
+		ResponseBase response = new ResponseBase();
+		try {
+			
+			Unit unit = entity.data;
+			
+			unitservice.save(unit);
+			
+			response.setRespcode(ResponseMessage.KEY_RC_SUCCESS);
+			response.setMessage(ResponseMessage.getMessage(ResponseMessage.KEY_RC_SUCCESS));
+			return new ResponseEntity<ResponseBase>(response,HttpStatus.OK);
+		}catch (Exception e) {
+			response.setRespcode(ResponseMessage.KEY_RC_SERVER_ERROR);
+			response.setMessage(e.getMessage());
+		    return new ResponseEntity<ResponseBase>(response,HttpStatus.OK);
+		}
+	}
+	
+	@RequestMapping(value = "/deleteUnit",method = RequestMethod.POST)
+	public ResponseEntity<ResponseBase> DeleteUnit(@RequestBody Unit_delete_request entity, HttpServletRequest request ) {//@RequestParam("type") 
+		ResponseBase response = new ResponseBase();
+		try {
+			unitservice.deleteById(entity.id);
+			response.setRespcode(ResponseMessage.KEY_RC_SUCCESS);
+			response.setMessage(ResponseMessage.getMessage(ResponseMessage.KEY_RC_SUCCESS));
+			return new ResponseEntity<ResponseBase>(response,HttpStatus.OK);
+		}catch (Exception e) {
+			response.setRespcode(ResponseMessage.KEY_RC_SERVER_ERROR);
+			response.setMessage(e.getMessage());
+		    return new ResponseEntity<ResponseBase>(response,HttpStatus.OK);
+		}
+	}
+	
 }
