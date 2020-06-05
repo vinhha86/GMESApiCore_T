470d0dbef90f3169cb44a67875d9219df04d012c
hungdaibang <nthung.dscvn@gmail.com>
2020-06-05 15:24:32 +0700

    PO

--- src/main/java/vn/gpay/gsmart/core/pcontract_po/IPContract_POService.java
+++ src/main/java/vn/gpay/gsmart/core/pcontract_po/IPContract_POService.java
@@ -7,5 +7,7 @@ import vn.gpay.gsmart.core.base.Operations;
 
 public interface IPContract_POService extends Operations<PContract_PO> {
 
-	List<PContract_PO> getPriceByContract(Long orgrootid_link, Long pcontractid_link, Long productid_link);
+	List<PContract_PO> getPOByContractProduct(Long orgrootid_link, Long pcontractid_link, Long productid_link);
+
+	List<PContract_PO> getPOByContract(Long orgrootid_link, Long pcontractid_link);
 }
