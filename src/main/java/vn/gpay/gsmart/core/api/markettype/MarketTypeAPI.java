af6492740dcfb3a8a283ebfda9bc5ebeb54fbf78
Ha Nguyen Vinh <vinhha2701@gmail.com>
2020-08-27 16:30:06 +0700

    update markettype api

--- src/main/java/vn/gpay/gsmart/core/api/markettype/MarketTypeAPI.java
+++ src/main/java/vn/gpay/gsmart/core/api/markettype/MarketTypeAPI.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import vn.gpay.gsmart.core.base.ResponseBase;
 import vn.gpay.gsmart.core.markettype.IMarketTypeService;
 import vn.gpay.gsmart.core.markettype.MarketType;
 import vn.gpay.gsmart.core.utils.ResponseMessage;
@@ -22,7 +23,7 @@ public class MarketTypeAPI {
 	@Autowired IMarketTypeService marketService;
 	
 	@RequestMapping(value = "/getall", method = RequestMethod.POST)
-	public ResponseEntity<Market_getall_response> Product_GetAll(HttpServletRequest request,
+	public ResponseEntity<Market_getall_response> MarketType_GetAll(HttpServletRequest request,
 			@RequestBody MarketService_getall_request entity) {
 		Market_getall_response response = new Market_getall_response();
 		try {
@@ -39,4 +40,36 @@ public class MarketTypeAPI {
 		}
 	}
 	
+	@RequestMapping(value = "/createMarketType",method = RequestMethod.POST)
+	public ResponseEntity<ResponseBase> CreateMarketType(@RequestBody MarketTypeRequest entity, HttpServletRequest request ) {//@RequestParam("type") 
+		ResponseBase response = new ResponseBase();
+		try {
+			MarketType mt = entity.data;
+			marketService.save(mt);
+			response.setRespcode(ResponseMessage.KEY_RC_SUCCESS);
+			response.setMessage(ResponseMessage.getMessage(ResponseMessage.KEY_RC_SUCCESS));
+			return new ResponseEntity<ResponseBase>(response,HttpStatus.OK);
+		}catch (Exception e) {
+			response.setRespcode(ResponseMessage.KEY_RC_SERVER_ERROR);
+			response.setMessage(e.getMessage());
+		    return new ResponseEntity<ResponseBase>(response,HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@RequestMapping(value = "/deleteMarketType",method = RequestMethod.POST)
+	public ResponseEntity<ResponseBase> DeleteMarketType(@RequestBody MarketTypeRequest entity, HttpServletRequest request ) {//@RequestParam("type") 
+		ResponseBase response = new ResponseBase();
+		try {
+			MarketType mt = marketService.findOne(entity.id);
+			mt.setStatus(-1);
+			marketService.save(mt);
+			response.setRespcode(ResponseMessage.KEY_RC_SUCCESS);
+			response.setMessage(ResponseMessage.getMessage(ResponseMessage.KEY_RC_SUCCESS));
+			return new ResponseEntity<ResponseBase>(response,HttpStatus.OK);
+		}catch (Exception e) {
+			response.setRespcode(ResponseMessage.KEY_RC_SERVER_ERROR);
+			response.setMessage(e.getMessage());
+		    return new ResponseEntity<ResponseBase>(response,HttpStatus.BAD_REQUEST);
+		}
+	}
 }
