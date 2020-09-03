b9ae683f01e35b78e10e27443f104c962f654f89
Ha Nguyen Vinh <vinhha2701@gmail.com>
2020-09-03 12:35:43 +0700

    update packingtype api

--- src/main/java/vn/gpay/gsmart/core/api/packingtype/PackingTypeAPI.java
+++ src/main/java/vn/gpay/gsmart/core/api/packingtype/PackingTypeAPI.java
@@ -8,9 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import vn.gpay.gsmart.core.base.ResponseBase;
 import vn.gpay.gsmart.core.packingtype.IPackingTypeService;
 import vn.gpay.gsmart.core.packingtype.PackingType;
 import vn.gpay.gsmart.core.security.GpayUser;
@@ -24,7 +27,7 @@ public class PackingTypeAPI {
 	IPackingTypeService packingtypeService;
 	
 	@RequestMapping(value = "/getall", method = RequestMethod.POST)
-	public ResponseEntity<PackingType_getall_response> Product_GetAll(HttpServletRequest request) {
+	public ResponseEntity<PackingType_getall_response>PackingType_GetAll(HttpServletRequest request) {
 		PackingType_getall_response response = new PackingType_getall_response();
 		try {
 			GpayUser user = (GpayUser) SecurityContextHolder.getContext().getAuthentication()
@@ -39,6 +42,46 @@ public class PackingTypeAPI {
 			response.setRespcode(ResponseMessage.KEY_RC_EXCEPTION);
 			response.setMessage(e.getMessage());
 			return new ResponseEntity<PackingType_getall_response>(response, HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@RequestMapping(value = "/createPackingType",method = RequestMethod.POST)
+	public ResponseEntity<ResponseBase> CreatePackingType(@RequestBody PackingTypeRequest entity, HttpServletRequest request ) {//@RequestParam("type") 
+		ResponseBase response = new ResponseBase();
+		try {
+			GpayUser user = (GpayUser) SecurityContextHolder.getContext().getAuthentication()
+					.getPrincipal();
+			PackingType pt = entity.data;
+			if(pt.getId() == null || pt.getId() == 0) {
+				pt.setOrgrootid_link(user.getRootorgid_link());
+			}
+			packingtypeService.save(pt);
+			response.setRespcode(ResponseMessage.KEY_RC_SUCCESS);
+			response.setMessage(ResponseMessage.getMessage(ResponseMessage.KEY_RC_SUCCESS));
+			return new ResponseEntity<ResponseBase>(response,HttpStatus.OK);
+		}catch (Exception e) {
+			response.setRespcode(ResponseMessage.KEY_RC_SERVER_ERROR);
+			response.setMessage(e.getMessage());
+		    return new ResponseEntity<ResponseBase>(response,HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@RequestMapping(value = "/deletePackingType",method = RequestMethod.POST)
+	public ResponseEntity<ResponseBase> DeletePackingType(@RequestBody PackingTypeRequest entity, HttpServletRequest request ) {//@RequestParam("type") 
+		ResponseBase response = new ResponseBase();
+		try {
+//			PackingType pt = packingtypeService.findOne(entity.id);
+//			pt.setStatus(-1);
+//			packingtypeService.save(pt);
+			packingtypeService.deleteById(entity.id);
+			
+			response.setRespcode(ResponseMessage.KEY_RC_SUCCESS);
+			response.setMessage(ResponseMessage.getMessage(ResponseMessage.KEY_RC_SUCCESS));
+			return new ResponseEntity<ResponseBase>(response,HttpStatus.OK);
+		}catch (Exception e) {
+			response.setRespcode(ResponseMessage.KEY_RC_SERVER_ERROR);
+			response.setMessage(e.getMessage());
+		    return new ResponseEntity<ResponseBase>(response,HttpStatus.BAD_REQUEST);
 		}
 	}
 }
