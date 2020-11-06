93b934c18279c2fde4f03921e705c108328e61e0
Phong <phongngh91@gmail.com>
2020-11-06 17:13:34 +0700

    update

--- src/main/java/vn/gpay/gsmart/core/porder_grant/POrderGrant.java
+++ src/main/java/vn/gpay/gsmart/core/porder_grant/POrderGrant.java
@@ -179,7 +179,7 @@ public class POrderGrant implements Serializable {
 		
 		if(porder != null) {
 			float totalPO = porder.getPo_quantity() == null ? 0 : porder.getPo_quantity();
-			String PO = porder.getPo_buyer() == null ? "" : porder.getPo_vendor();
+			String PO = porder.getPo_buyer() == null ? porder.getPo_vendor() : porder.getPo_buyer();
 			name += ""+code+"/"+PO+"/"+decimalFormat.format(total)+" / "+decimalFormat.format(totalPO);
 		}
 		
@@ -197,7 +197,7 @@ public class POrderGrant implements Serializable {
 		
 		if(porder != null) {
 			float totalPO = porder.getPo_quantity() == null ? 0 : porder.getPo_quantity();
-			String PO = porder.getPo_buyer() == null ? "" : porder.getPo_vendor();
+			String PO = porder.getPo_buyer() == null ? porder.getPo_vendor() : porder.getPo_buyer();
 			name += "#"+code+"/"+PO+"/"+decimalFormat.format(total)+"/"+decimalFormat.format(totalPO);
 		}
 		
