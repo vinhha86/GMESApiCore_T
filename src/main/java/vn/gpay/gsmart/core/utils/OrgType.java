959b85c3e99e6cf27891c9d00cde655e81256795
hungdaibang <nthung.dscvn@gmail.com>
2020-12-02 08:55:52 +0700

    orgtype

---
+++ src/main/java/vn/gpay/gsmart/core/utils/OrgType.java
@@ -0,0 +1,11 @@
+package vn.gpay.gsmart.core.utils;
+
+public class OrgType {
+
+	public static int ORG_TYPE_VENDOR    		= 11;
+    public static int ORG_TYPE_BUYER    		= 12; 
+	public static int ORG_TYPE_FACTORY    		= 13;//phan xuong
+	public static int ORG_TYPE_LINE    			= 14;//to chuyen
+
+    public static int ORG_TYPE_UNKNOWN   		= -1; //Không xác định
+}
