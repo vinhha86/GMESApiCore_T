06b3dfdba80433750214da061fe5f96bd37422e8
Phong Thần <napoleon.ptit@gmail.com>
2021-08-10 19:17:22 +0700

    update multi thread load bom

--- src/main/java/vn/gpay/gsmart/core/api/pcontractproductbom/PContractProductBom2API.java
+++ src/main/java/vn/gpay/gsmart/core/api/pcontractproductbom/PContractProductBom2API.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -38,6 +40,7 @@ import vn.gpay.gsmart.core.pcontractproduct.IPContractProductService;
 import vn.gpay.gsmart.core.pcontractproduct.PContractProduct;
 import vn.gpay.gsmart.core.pcontractproductbom.IPContractProductBom2Service;
 import vn.gpay.gsmart.core.pcontractproductbom.PContractProductBom2;
+import vn.gpay.gsmart.core.pcontractproductbom.PContractProductBom2_Runnable;
 import vn.gpay.gsmart.core.pcontractproductsku.IPContractProductSKUService;
 import vn.gpay.gsmart.core.pcontractproductsku.PContractProductSKU;
 import vn.gpay.gsmart.core.productpairing.IProductPairingService;
@@ -964,6 +967,180 @@ public class PContractProductBom2API {
 		return new ResponseEntity<get_bom_by_product_response>(response, HttpStatus.OK);
 	}
 	
+	@RequestMapping(value = "/getbom_by_product_multithread", method = RequestMethod.POST)
+	public ResponseEntity<get_bom_by_product_response> GetBomByProductMultiThread(HttpServletRequest request,
+			@RequestBody get_bom_by_product_request entity) {
+		get_bom_by_product_response response = new get_bom_by_product_response();
+		try {
+			GpayUser user = (GpayUser) SecurityContextHolder.getContext().getAuthentication()
+					.getPrincipal();
+			long orgrootid_link = user.getRootorgid_link();
+			long pcontractid_link = entity.pcontractid_link;
+			long productid_link = entity.productid_link;
+			long colorid_link = 0;
+			List<Map<String, String>> listdata = new ArrayList<Map<String, String>>();		
+			
+			List<Long> list_colorid = ppskuService.getlistvalue_by_product(pcontractid_link, productid_link, AtributeFixValues.ATTR_COLOR);
+			
+			List<PContractProductBom2> listbom = ppbom2service.get_pcontract_productBOMbyid(productid_link, pcontractid_link);
+//			List<PContractBom2Color> listbomcolor = ppbomcolor2service.getall_material_in_productBOMColor(pcontractid_link, productid_link, colorid_link, (long)0);
+			List<PContractBOM2SKU> listbomsku = ppbom2skuservice.getmaterial_bycolorid_link(pcontractid_link, productid_link, colorid_link, 0);
+			
+			List<Long> List_size = ppskuService.getlistvalue_by_product(pcontractid_link, productid_link, (long)30);
+//			List<Long> List_size = ppatt_service.getvalueid_by_product_and_pcontract_and_attribute(orgrootid_link, pcontractid_link, productid_link, (long) 30);
+					//ppbomskuservice.getsize_bycolor(pcontractid_link, productid_link, colorid_link);
+			
+			ExecutorService executor = Executors.newFixedThreadPool(listbom.size()+1);
+			for (PContractProductBom2 pContractProductBom : listbom) {
+				Runnable bom2 = new PContractProductBom2_Runnable(productid_link, pcontractid_link, pContractProductBom, list_colorid, List_size, listbomsku, listdata, po_npl_Service, 
+						avService, ppbom2skuservice);
+				executor.execute(bom2);
+			}
+			executor.shutdown();
+			// Wait until all threads are finish
+			while (!executor.isTerminated()) {
+	 
+			}
+			
+			//lay trang thai cua dinh muc
+			List<PContractProduct> list_product = ppService.get_by_product_and_pcontract(orgrootid_link, productid_link, pcontractid_link);
+			if(list_product.size() > 0) {
+				response.isbomdone = list_product.get(0).getIsbom2done() == null ? false : list_product.get(0).getIsbom2done();
+			}
+			
+			response.data = listdata;
+			response.setRespcode(ResponseMessage.KEY_RC_SUCCESS);
+			response.setMessage(ResponseMessage.getMessage(ResponseMessage.KEY_RC_SUCCESS));
+		} catch (Exception e) {
+			response.setRespcode(ResponseMessage.KEY_RC_EXCEPTION);
+			response.setMessage(e.getMessage());
+		}
+		return new ResponseEntity<get_bom_by_product_response>(response, HttpStatus.OK);
+	}
+	
+//	private static class PContractProductBom2_Runnable implements Runnable{
+//		private final IPContract_bom2_npl_poline_Service po_npl_Service; 
+//		private final IAttributeValueService avService;
+//		private final IPContractBOM2SKUService ppbom2skuservice;
+//		
+//		private final Long productid_link;
+//		private final Long pcontractid_link;
+//		private final PContractProductBom2 pContractProductBom;
+//		private final List<Long> list_colorid;
+//		private final List<Long> List_size;
+//		private final List<PContractBOM2SKU> listbomsku;
+//		private final List<Map<String, String>> listdata;
+//		
+//		PContractProductBom2_Runnable(Long productid_link, Long pcontractid_link, PContractProductBom2 pContractProductBom, List<Long> list_colorid, List<Long> List_size,
+//				List<PContractBOM2SKU> listbomsku, List<Map<String, String>> listdata, IPContract_bom2_npl_poline_Service po_npl_Service, IAttributeValueService avService,
+//				IPContractBOM2SKUService ppbom2skuservice) {
+//			// TODO Auto-generated constructor stub
+//			this.productid_link = productid_link;
+//			this.pcontractid_link = pcontractid_link;
+//			this.pContractProductBom = pContractProductBom;
+//			this.list_colorid = list_colorid;
+//			this.List_size = List_size;
+//			this.listdata = listdata;
+//			this.listbomsku = listbomsku;
+//			this.po_npl_Service = po_npl_Service;
+//			this.avService = avService;
+//			this.ppbom2skuservice = ppbom2skuservice;
+//		}
+//
+//		@Override
+//		public void run() {
+//			// TODO Auto-generated method stub
+//			List<PContract_bom2_npl_poline> list_po = po_npl_Service.getby_product_and_npl(productid_link, pcontractid_link, pContractProductBom.getMaterialid_link());
+//			String str_po = "";
+//			for(PContract_bom2_npl_poline po_npl : list_po) {
+//				if(str_po == "") {
+//					str_po = po_npl.getPO_Buyer();
+//				}
+//				else {
+//					str_po += ", "+ po_npl.getPO_Buyer();
+//				}
+//				
+//			}
+//			
+//			for(Long colorid : list_colorid) {
+//				Map<String, String> map = new HashMap<String, String>();
+//									
+//				map.put("coKho", pContractProductBom.getCoKho().replace("ALL, ", "").replace(", ALL", "").replace("ALL", "")+"");
+//				
+//				map.put("createddate", pContractProductBom.getCreateddate()+"");
+//				
+//				map.put("createduserid_link", "0"+pContractProductBom.getCreateduserid_link());
+//				
+//				map.put("description", pContractProductBom.getDescription_product()+"");
+//				
+//				map.put("id", "0"+pContractProductBom.getId());
+//				
+//				map.put("lost_ratio", "0"+pContractProductBom.getLost_ratio());
+//				
+//				map.put("materialid_link", "0"+pContractProductBom.getMaterialid_link());
+//				
+//				map.put("materialName", pContractProductBom.getMaterialName()+"");
+//				
+//				map.put("materialCode", pContractProductBom.getMaterialCode()+"");
+//				
+//				map.put("orgrootid_link", "0"+pContractProductBom.getOrgrootid_link());
+//				
+//				map.put("pcontractid_link", "0"+pContractProductBom.getPcontractid_link());
+//				
+//				map.put("product_type", pContractProductBom.getProduct_type()+"");
+//				
+//				map.put("product_typename", pContractProductBom.getProduct_typeName()+"");
+//				
+//				map.put("productid_link", pContractProductBom.getProductid_link()+"");
+//				
+//				map.put("tenMauNPL", pContractProductBom.getTenMauNPL()+"");
+//				
+//				map.put("thanhPhanVai", pContractProductBom.getDescription_product()+"");
+//				
+//				map.put("unitName", pContractProductBom.getUnitName()+"");
+//				
+//				map.put("unitid_link", "0"+pContractProductBom.getUnitid_link());
+//				
+//				map.put("colorid_link", "0"+colorid);
+//				
+//				map.put("nhacungcap", pContractProductBom.getNhaCungCap());
+//				
+//				map.put("po_line", str_po);
+//				
+//				Attributevalue value = avService.findOne(colorid);
+//				String color_name = value.getValue();
+//				map.put("color_name", ""+color_name);
+//				
+//				Float total_amount = (float) 0;
+//				int total_size=0;
+//				
+//				boolean check = false;
+//				for(Long size : List_size) {
+//					List<PContractBOM2SKU> listbomsku_clone = new ArrayList<PContractBOM2SKU>(listbomsku);
+//					long skuid_link = ppbom2skuservice.getskuid_link_by_color_and_size(colorid, size, productid_link);
+//					listbomsku_clone.removeIf(c -> !c.getMaterial_skuid_link().equals(pContractProductBom.getMaterialid_link()) || 
+//							!c.getProduct_skuid_link().equals(skuid_link));
+//					Float amount_size = (float) 0;
+//					if(listbomsku_clone.size() > 0)
+//						amount_size = listbomsku_clone.get(0).getAmount();
+//					map.put(""+size, amount_size+"");
+//					
+//					if (amount_size>0){
+//						check = true;
+//						total_amount += amount_size;
+//						total_size++;
+//					}
+//				}
+//				
+//				if (total_size>0)
+//					map.put("amount", "0"+(total_amount/total_size));
+//				else
+//					map.put("amount", "0");
+//				if(check)
+//					listdata.add(map);	
+//			}
+//		}
+//	}
 	
 	public List<Map<String, String>> GetListProductBomColor(PContractProductBOM_getbomcolor_request entity) {
 		PContractProductBOM_getbomcolor_response response = new PContractProductBOM_getbomcolor_response();
