e6df475d5818242fc4068abdb763d75e6622f713
Phong Thần <napoleon.ptit@gmail.com>
2021-09-20 20:02:38 +0700

    Update POrder_Req_Service.java

--- src/main/java/vn/gpay/gsmart/core/porder_req/POrder_Req_Service.java
+++ src/main/java/vn/gpay/gsmart/core/porder_req/POrder_Req_Service.java
@@ -19,110 +19,109 @@ import vn.gpay.gsmart.core.utils.GPAYDateFormat;
 
 @Service
 public class POrder_Req_Service extends AbstractService<POrder_Req> implements IPOrder_Req_Service {
-	@Autowired IPOrder_Req_Repository repo;
+	@Autowired
+	IPOrder_Req_Repository repo;
+
 	protected JpaRepository<POrder_Req, Long> getRepository() {
 		// TODO Auto-generated method stub
 		return repo;
 	}
-	
+
 	@Override
-	public List<POrder_Req> getByContract(Long pcontractid_link){
+	public List<POrder_Req> getByContract(Long pcontractid_link) {
 		return repo.getByContract(pcontractid_link);
 	}
-	
+
 	@Override
-	public Long savePOrder_Req(POrder_Req porder_req){
+	public Long savePOrder_Req(POrder_Req porder_req) {
 		try {
 			porder_req = this.save(porder_req);
 			return porder_req.getId();
-		} catch (Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
-	
-	@Override
-	public List<POrder_Req> getByContractAndProduct(Long pcontractid_link, Long productid_link){
-		return repo.getByContractAndProduct(pcontractid_link,productid_link);
-	}
-	@Override
-	public List<POrder_Req> getByPOAndProduct(Long pcontract_poid_link, Long productid_link){
-		return repo.getByPOAndProduct(pcontract_poid_link,productid_link);
-	}
-	
-	@Override
-	public List<POrder_Req> getByContractAndPO(Long pcontractid_link, Long pcontract_poid_link){
-		return repo.getByContractAndPO(pcontractid_link,pcontract_poid_link);
-	}
-	
-	@Override
-	public List<POrder_Req> getByPO(Long pcontract_poid_link){
-		return repo.getByPO(pcontract_poid_link);
-	}
-	
-	@Override
-	public List<POrder_Req> getByOrg_PO_Product(Long pcontract_poid_link, long productid_link, long granttoorgid_link){
-		return repo.getByOrg_PO_Product(pcontract_poid_link, productid_link, granttoorgid_link);	
-	} 
-	
-	@Override
-	public List<POrder_Req> getByStatus(Integer status){
-		return repo.getByStatus(status);
-	}
-	
 
 	@Override
-	public List<POrder_Req> getFilter(
-			String ordercode,
-			Integer status,
-			Long granttoorgid_link,
-			String collection, 
-			String season, 
-			Integer salaryyear,
-			Integer salarymonth,
-			Date processingdate_from,
-			Date processingdate_to
-			) {
+	public List<POrder_Req> getByContractAndProduct(Long pcontractid_link, Long productid_link) {
+		return repo.getByContractAndProduct(pcontractid_link, productid_link);
+	}
+
+	@Override
+	public List<POrder_Req> getByPOAndProduct(Long pcontract_poid_link, Long productid_link) {
+		return repo.getByPOAndProduct(pcontract_poid_link, productid_link);
+	}
+
+	@Override
+	public List<POrder_Req> getByContractAndPO(Long pcontractid_link, Long pcontract_poid_link) {
+		return repo.getByContractAndPO(pcontractid_link, pcontract_poid_link);
+	}
+
+	@Override
+	public List<POrder_Req> getByPO(Long pcontract_poid_link) {
+		return repo.getByPO(pcontract_poid_link);
+	}
+
+	@Override
+	public List<POrder_Req> getByOrg_PO_Product(Long pcontract_poid_link, long productid_link, long granttoorgid_link) {
+		return repo.getByOrg_PO_Product(pcontract_poid_link, productid_link, granttoorgid_link);
+	}
+
+	@Override
+	public List<POrder_Req> getByStatus(Integer status) {
+		return repo.getByStatus(status);
+	}
+
+	@Override
+	public List<POrder_Req> getFilter(String ordercode, Integer status, Long granttoorgid_link, String collection,
+			String season, Integer salaryyear, Integer salarymonth, Date processingdate_from, Date processingdate_to) {
 		try {
-			if (null != salarymonth){
+			if (null != salarymonth) {
 				Specification<POrder_Req> specification = Specifications.<POrder_Req>and()
-						.eq(null!=status && status !=-1, "status", status)
-			            .eq(null!=granttoorgid_link && granttoorgid_link != -1, "granttoorgid_link", granttoorgid_link)
-			            .like(null!=ordercode && ordercode !="", "ordercode", "%"+ordercode+"%")
-			            .like(null!=collection && collection !="", "collection", "%"+collection+"%")
-			            .like(null!=season && season !="", "season", "%"+season+"%")
-			            .eq(Objects.nonNull(salaryyear), "salaryyear", salaryyear)
-			            .eq(null!=salarymonth && salarymonth != -1, "salarymonth", salarymonth)
-			            .ge(Objects.nonNull(processingdate_from),"orderdate",GPAYDateFormat.atStartOfDay(processingdate_from))
-		                .le(Objects.nonNull(processingdate_to),"orderdate",GPAYDateFormat.atEndOfDay(processingdate_to))
-		                .between(processingdate_from!=null && processingdate_to!=null,"orderdate", GPAYDateFormat.atStartOfDay(processingdate_from), GPAYDateFormat.atEndOfDay(processingdate_to))
-			            .build();
-				Sort sort = Sorts.builder()
-				        .desc("ordercode")
-				        .build();
-				List<POrder_Req> a = repo.findAll(specification,sort);
+						.eq(null != status && status != -1, "status", status)
+						.eq(null != granttoorgid_link && granttoorgid_link != -1, "granttoorgid_link",
+								granttoorgid_link)
+						.like(null != ordercode && ordercode != "", "ordercode", "%" + ordercode + "%")
+						.like(null != collection && collection != "", "collection", "%" + collection + "%")
+						.like(null != season && season != "", "season", "%" + season + "%")
+						.eq(Objects.nonNull(salaryyear), "salaryyear", salaryyear)
+						.eq(null != salarymonth && salarymonth != -1, "salarymonth", salarymonth)
+						.ge(Objects.nonNull(processingdate_from), "orderdate",
+								GPAYDateFormat.atStartOfDay(processingdate_from))
+						.le(Objects.nonNull(processingdate_to), "orderdate",
+								GPAYDateFormat.atEndOfDay(processingdate_to))
+						.between(processingdate_from != null && processingdate_to != null, "orderdate",
+								GPAYDateFormat.atStartOfDay(processingdate_from),
+								GPAYDateFormat.atEndOfDay(processingdate_to))
+						.build();
+				Sort sort = Sorts.builder().desc("ordercode").build();
+				List<POrder_Req> a = repo.findAll(specification, sort);
 				return a;
 			} else {
 				Specification<POrder_Req> specification = Specifications.<POrder_Req>and()
-						.eq(null!=status && status !=-1, "status", status)
-			            .eq(null!=granttoorgid_link && granttoorgid_link != -1, "granttoorgid_link", granttoorgid_link)
-			            .like(null!=ordercode && ordercode !="", "ordercode", "%"+ordercode+"%")
-			            .like(null!=collection && collection !="", "collection", "%"+collection+"%")
-			            .like(null!=season && season !="", "season", "%"+season+"%")
-			            .eq(Objects.nonNull(salaryyear), "salaryyear", salaryyear)
+						.eq(null != status && status != -1, "status", status)
+						.eq(null != granttoorgid_link && granttoorgid_link != -1, "granttoorgid_link",
+								granttoorgid_link)
+						.like(null != ordercode && ordercode != "", "ordercode", "%" + ordercode + "%")
+						.like(null != collection && collection != "", "collection", "%" + collection + "%")
+						.like(null != season && season != "", "season", "%" + season + "%")
+						.eq(Objects.nonNull(salaryyear), "salaryyear", salaryyear)
 //			            .eq(null!=salarymonth && salarymonth != -1, "salarymonth", salarymonth)
-			            .eq("salarymonth", salarymonth)
-			            .ge(Objects.nonNull(processingdate_from),"orderdate",GPAYDateFormat.atStartOfDay(processingdate_from))
-		                .le(Objects.nonNull(processingdate_to),"orderdate",GPAYDateFormat.atEndOfDay(processingdate_to))
-		                .between(processingdate_from!=null && processingdate_to!=null,"orderdate", GPAYDateFormat.atStartOfDay(processingdate_from), GPAYDateFormat.atEndOfDay(processingdate_to))
-			            .build();
-				Sort sort = Sorts.builder()
-				        .desc("ordercode")
-				        .build();
-				List<POrder_Req> a = repo.findAll(specification,sort);
+						.eq("salarymonth", salarymonth)
+						.ge(Objects.nonNull(processingdate_from), "orderdate",
+								GPAYDateFormat.atStartOfDay(processingdate_from))
+						.le(Objects.nonNull(processingdate_to), "orderdate",
+								GPAYDateFormat.atEndOfDay(processingdate_to))
+						.between(processingdate_from != null && processingdate_to != null, "orderdate",
+								GPAYDateFormat.atStartOfDay(processingdate_from),
+								GPAYDateFormat.atEndOfDay(processingdate_to))
+						.build();
+				Sort sort = Sorts.builder().desc("ordercode").build();
+				List<POrder_Req> a = repo.findAll(specification, sort);
 				return a;
 			}
-		} catch(Exception ex){
+		} catch (Exception ex) {
 			ex.printStackTrace();
 			return null;
 		}
@@ -143,33 +142,26 @@ public class POrder_Req_Service extends AbstractService<POrder_Req> implements I
 //		return a.size() > 0 ? a : new ArrayList<POrder_Req>();
 		vendors = vendors.size() == 0 ? null : vendors;
 		buyers = buyers.size() == 0 ? null : buyers;
-		return repo.getByOrg(orgid_link,vendors , buyers);
+		return repo.getByOrg(orgid_link, vendors, buyers);
 	}
-	
+
 	@Override
 	public List<POrder_Req> get_req_granted(long orgid_link) {
 		// TODO Auto-generated method stub
-		Specification<POrder_Req> specification = Specifications.<POrder_Req>and()
-				.eq("granttoorgid_link", orgid_link)
-				.eq("status", 1)
-				.le("pcontract_po.status", -1)
-	            .build();
-		Sort sort = Sorts.builder()
-		        .desc("id")
-		        .build();
-		List<POrder_Req> a = repo.findAll(specification,sort);
+		Specification<POrder_Req> specification = Specifications.<POrder_Req>and().eq("granttoorgid_link", orgid_link)
+				.eq("status", 1).le("pcontract_po.status", -1).build();
+		Sort sort = Sorts.builder().desc("id").build();
+		List<POrder_Req> a = repo.findAll(specification, sort);
 		return a.size() > 0 ? a : new ArrayList<POrder_Req>();
 	}
-	
+
 	@Override
-	//Danh sach cac lenh duoc phan cho Phan xuong nhung chua duoc phan chuyen
-	public List<POrder_Req> get_free_bygolivedate(Date golivedate_from, Date golivedate_to, Long granttoorgid_link){
-		Specification<POrder_Req> specification = Specifications.<POrder_Req>and()
-				.eq("status", 0)
-	            .eq("granttoorgid_link", granttoorgid_link)
-	            .ge(Objects.nonNull(golivedate_from),"golivedate",GPAYDateFormat.atStartOfDay(golivedate_from))
-                .le(Objects.nonNull(golivedate_to),"golivedate",GPAYDateFormat.atEndOfDay(golivedate_to))
-	            .build();
+	// Danh sach cac lenh duoc phan cho Phan xuong nhung chua duoc phan chuyen
+	public List<POrder_Req> get_free_bygolivedate(Date golivedate_from, Date golivedate_to, Long granttoorgid_link) {
+		Specification<POrder_Req> specification = Specifications.<POrder_Req>and().eq("status", 0)
+				.eq("granttoorgid_link", granttoorgid_link)
+				.ge(Objects.nonNull(golivedate_from), "golivedate", GPAYDateFormat.atStartOfDay(golivedate_from))
+				.le(Objects.nonNull(golivedate_to), "golivedate", GPAYDateFormat.atEndOfDay(golivedate_to)).build();
 //		Sort sort = Sorts.builder()
 //		        .desc("ordercode")
 //		        .build();
@@ -188,7 +180,8 @@ public class POrder_Req_Service extends AbstractService<POrder_Req> implements I
 	public List<POrder_Req> getByContractAndPO_and_Org(Long pcontractid_link, Long pcontract_poid_link,
 			Long granttoorgid_link, Long productid_link) {
 		// TODO Auto-generated method stub
-		return repo.getByContractAndPO_and_org(pcontractid_link, pcontract_poid_link, granttoorgid_link, productid_link);
+		return repo.getByContractAndPO_and_org(pcontractid_link, pcontract_poid_link, granttoorgid_link,
+				productid_link);
 	}
 
 	@Override
@@ -213,5 +206,11 @@ public class POrder_Req_Service extends AbstractService<POrder_Req> implements I
 	public List<POrder_Req> getbyOffer_and_Product(Long pcontractpo_id_link, Long productid_link, Long orgid_link) {
 		// TODO Auto-generated method stub
 		return repo.getByOfferAndProduct(pcontractpo_id_link, productid_link, orgid_link);
+	}
+
+	@Override
+	public List<Long> getOrgbyPContractAndProduct(Long pcontractid_link, Long productid_link) {
+		// TODO Auto-generated method stub
+		return repo.getOrgByPContractAndProduct(pcontractid_link, productid_link);
 	}
 }
