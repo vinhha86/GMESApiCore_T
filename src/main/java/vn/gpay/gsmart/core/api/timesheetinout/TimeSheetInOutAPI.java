9f35a7eaac6668938495aebf8500a70187dff9ca
Phong <phongngh91@gmail.com>
2022-01-13 08:57:10 +0700

    update

--- src/main/java/vn/gpay/gsmart/core/api/timesheetinout/TimeSheetInOutAPI.java
+++ src/main/java/vn/gpay/gsmart/core/api/timesheetinout/TimeSheetInOutAPI.java
@@ -1,6 +1,11 @@
 package vn.gpay.gsmart.core.api.timesheetinout;
 
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,23 +15,64 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import vn.gpay.gsmart.core.timesheetinout.ITimeSheetInOutService;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import vn.gpay.gsmart.core.personel.IPersonnel_Service;
+import vn.gpay.gsmart.core.personel.Personel;
 import vn.gpay.gsmart.core.timesheetinout.TimeSheetInOut;
+import vn.gpay.gsmart.core.utils.AtributeFixValues;
 import vn.gpay.gsmart.core.utils.ResponseMessage;
 
 @RestController
 @RequestMapping("/api/v1/timesheetinout")
 public class TimeSheetInOutAPI {
-	@Autowired ITimeSheetInOutService timesheetinoutService;
+	@Autowired
+	IPersonnel_Service personService;
 	//lấy tất cả  danh sách 
 	@RequestMapping(value = "/getall", method = RequestMethod.POST)
 	public ResponseEntity<TimeSheetInOut_load_response> timesheetinout_GetAll(@RequestBody TimeSheetInOut_load_request entity) {
 		TimeSheetInOut_load_response response = new TimeSheetInOut_load_response();
 		try {
-
-			List<TimeSheetInOut> lsttimesheetinout =timesheetinoutService.getAll(entity.todate, entity.fromdate);
+			String urlPush = AtributeFixValues.url_timesheet+"/timesheet/getlist";
+			URL url = new URL(urlPush);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+//            conn.setRequestProperty("authorization", token);
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setRequestMethod("POST");
+            
+            ObjectMapper objectMapper = new ObjectMapper();
+            ObjectNode appParNode = objectMapper.createObjectNode();
+            
+//            appParNode.put("userid", entity.user.getId());
+//            appParNode.put("enable", enabled);
+            String jsonReq = objectMapper.writeValueAsString(appParNode);
+            
+            OutputStream os = conn.getOutputStream();
+            os.write(jsonReq.getBytes());
+            os.flush();
+                     
+            String result = "";
+			String line;
 			
-			System.out.println(lsttimesheetinout.size());
+            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            while ((line = rd.readLine()) != null) {
+            	result += line;
+            }
+            rd.close();
+            
+            conn.disconnect();
+
+			List<TimeSheetInOut> lsttimesheetinout = objectMapper.readValue(result, new TypeReference<List<TimeSheetInOut>>() {});
+			for(TimeSheetInOut inout : lsttimesheetinout) {
+				Personel person = personService.getPersonelBycode(inout.getPersonel_code());
+				inout.setFullname(person.getFullname());
+			}
 			response.data =lsttimesheetinout;
 			
 			response.setRespcode(ResponseMessage.KEY_RC_SUCCESS);
