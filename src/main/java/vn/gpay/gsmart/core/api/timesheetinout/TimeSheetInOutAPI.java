a65fa7947edb8fe08746e0e873579581d17b33d7
Phong Thần <napoleon.ptit@gmail.com>
2022-02-22 02:21:50 +0700

    timesheet month

--- src/main/java/vn/gpay/gsmart/core/api/timesheetinout/TimeSheetInOutAPI.java
+++ src/main/java/vn/gpay/gsmart/core/api/timesheetinout/TimeSheetInOutAPI.java
@@ -1,6 +1,5 @@
 package vn.gpay.gsmart.core.api.timesheetinout;
 
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -29,6 +28,7 @@ import vn.gpay.gsmart.core.personel.IPersonnel_Service;
 import vn.gpay.gsmart.core.personel.Personel;
 import vn.gpay.gsmart.core.timesheetinout.TimeSheetDaily;
 import vn.gpay.gsmart.core.timesheetinout.TimeSheetInOut;
+import vn.gpay.gsmart.core.timesheetinout.TimeSheetMonth;
 import vn.gpay.gsmart.core.utils.AtributeFixValues;
 import vn.gpay.gsmart.core.utils.HttpPost;
 import vn.gpay.gsmart.core.utils.ResponseMessage;
@@ -38,57 +38,61 @@ import vn.gpay.gsmart.core.utils.ResponseMessage;
 public class TimeSheetInOutAPI {
 	@Autowired
 	IPersonnel_Service personService;
-	//lấy tất cả  danh sách 
+
+	// lấy tất cả danh sách
 	@RequestMapping(value = "/getall", method = RequestMethod.POST)
-	public ResponseEntity<TimeSheetInOut_load_response> timesheetinout_GetAll(@RequestBody TimeSheetInOut_load_request entity) {
+	public ResponseEntity<TimeSheetInOut_load_response> timesheetinout_GetAll(
+			@RequestBody TimeSheetInOut_load_request entity) {
 		TimeSheetInOut_load_response response = new TimeSheetInOut_load_response();
 		try {
 			String date_from = entity.fromdate;
 			String date_to = entity.todate;
 			long orgid_link = entity.orgid_link;
-			
-			String urlPush = AtributeFixValues.url_timesheet+"/timesheet/getlist";
-			URL url = new URL(urlPush);
-			
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-//            conn.setRequestProperty("authorization", token);
-            conn.setRequestProperty("Content-Type", "application/json");
-            conn.setRequestProperty("Accept", "application/json");
-            conn.setRequestMethod("POST");
-            
-            ObjectMapper objectMapper = new ObjectMapper();
-            ObjectNode appParNode = objectMapper.createObjectNode();
-            
-            //truyen param theo
-            appParNode.put("date_from", date_from);
-            appParNode.put("date_to", date_to);
-            appParNode.put("orgid_link", orgid_link);
-            String jsonReq = objectMapper.writeValueAsString(appParNode);
-            
-            OutputStream os = conn.getOutputStream();
-            os.write(jsonReq.getBytes());
-            os.flush();
-                     
-            String result = "";
-			String line;
-			
-            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            while ((line = rd.readLine()) != null) {
-            	result += line;
-            }
-            rd.close();
-            
-            conn.disconnect();
 
-            DateFormat df_gio = new SimpleDateFormat("H:m:s");
-            DateFormat df2 = new SimpleDateFormat("yyyy-MM-dd H:m:s");
-            DateFormat df_ngay = new SimpleDateFormat("dd-MM-yyyy");
-			List<TimeSheetInOut> lsttimesheetinout = objectMapper.readValue(result, new TypeReference<List<TimeSheetInOut>>() {});
-			for(TimeSheetInOut inout : lsttimesheetinout) {
+			String urlPush = AtributeFixValues.url_timesheet + "/timesheet/getlist";
+			URL url = new URL(urlPush);
+
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+//            conn.setRequestProperty("authorization", token);
+			conn.setRequestProperty("Content-Type", "application/json");
+			conn.setRequestProperty("Accept", "application/json");
+			conn.setRequestMethod("POST");
+
+			ObjectMapper objectMapper = new ObjectMapper();
+			ObjectNode appParNode = objectMapper.createObjectNode();
+
+			// truyen param theo
+			appParNode.put("date_from", date_from);
+			appParNode.put("date_to", date_to);
+			appParNode.put("orgid_link", orgid_link);
+			String jsonReq = objectMapper.writeValueAsString(appParNode);
+
+			OutputStream os = conn.getOutputStream();
+			os.write(jsonReq.getBytes());
+			os.flush();
+
+			String result = "";
+			String line;
+
+			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			while ((line = rd.readLine()) != null) {
+				result += line;
+			}
+			rd.close();
+
+			conn.disconnect();
+
+			DateFormat df_gio = new SimpleDateFormat("H:m:s");
+			DateFormat df2 = new SimpleDateFormat("yyyy-MM-dd H:m:s");
+			DateFormat df_ngay = new SimpleDateFormat("dd-MM-yyyy");
+			List<TimeSheetInOut> lsttimesheetinout = objectMapper.readValue(result,
+					new TypeReference<List<TimeSheetInOut>>() {
+					});
+			for (TimeSheetInOut inout : lsttimesheetinout) {
 				Personel person = personService.getPersonelBycode(inout.getPersonel_code());
-				if(person!=null) {
+				if (person != null) {
 					inout.setFullname(person.getFullname());
 					inout.setPersonel_code(person.getCode());
 					String time = df_gio.format(df2.parse(inout.getTime()));
@@ -96,23 +100,23 @@ public class TimeSheetInOutAPI {
 					inout.setTime(time);
 					inout.setDay(ngay);
 				}
-					
+
 			}
 			lsttimesheetinout.removeIf(c -> c.getFullname() == null);
-			response.data =lsttimesheetinout;
-			
+			response.data = lsttimesheetinout;
+
 			response.setRespcode(ResponseMessage.KEY_RC_SUCCESS);
 			response.setMessage(ResponseMessage.getMessage(ResponseMessage.KEY_RC_SUCCESS));
-			
+
 		} catch (Exception e) {
 			response.setRespcode(ResponseMessage.KEY_RC_EXCEPTION);
 			response.setMessage(e.getMessage());
 			System.out.println(e.getMessage());
 		}
 		return new ResponseEntity<TimeSheetInOut_load_response>(response, HttpStatus.OK);
-		
+
 	}
-	
+
 	@RequestMapping(value = "/get_daily", method = RequestMethod.POST)
 	public ResponseEntity<getDailyResponse> getDaily(@RequestBody getDailyRequest entity) {
 		getDailyResponse response = new getDailyResponse();
@@ -121,68 +125,70 @@ public class TimeSheetInOutAPI {
 			int year = entity.year;
 			long grantid_link = entity.grantid_link;
 			long orgid_link = entity.orgid_link;
-			
-			String urlPush = AtributeFixValues.url_timesheet+"/timesheet/getlist_daily";
+
+			String urlPush = AtributeFixValues.url_timesheet + "/timesheet/getlist_daily";
 			URL url = new URL(urlPush);
-			
+
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
 //            conn.setRequestProperty("authorization", token);
-            conn.setRequestProperty("Content-Type", "application/json");
-            conn.setRequestProperty("Accept", "application/json");
-            conn.setRequestMethod("POST");
-            
-            ObjectMapper objectMapper = new ObjectMapper();
-            ObjectNode appParNode = objectMapper.createObjectNode();
-            
-            //truyen param theo
-            appParNode.put("month", month);
-            appParNode.put("year", year);
-            appParNode.put("orgid_link", orgid_link);
-            String jsonReq = objectMapper.writeValueAsString(appParNode);
-            
-            OutputStream os = conn.getOutputStream();
-            os.write(jsonReq.getBytes());
-            os.flush();
-                     
-            String result = "";
+			conn.setRequestProperty("Content-Type", "application/json");
+			conn.setRequestProperty("Accept", "application/json");
+			conn.setRequestMethod("POST");
+
+			ObjectMapper objectMapper = new ObjectMapper();
+			ObjectNode appParNode = objectMapper.createObjectNode();
+
+			// truyen param theo
+			appParNode.put("month", month);
+			appParNode.put("year", year);
+			appParNode.put("orgid_link", orgid_link);
+			String jsonReq = objectMapper.writeValueAsString(appParNode);
+
+			OutputStream os = conn.getOutputStream();
+			os.write(jsonReq.getBytes());
+			os.flush();
+
+			String result = "";
 			String line;
-			
-            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            while ((line = rd.readLine()) != null) {
-            	result += line;
-            }
-            rd.close();
-            
-            conn.disconnect();
-            
-			List<TimeSheetDaily> lsttimesheetinout = objectMapper.readValue(result, new TypeReference<List<TimeSheetDaily>>() {});
+
+			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			while ((line = rd.readLine()) != null) {
+				result += line;
+			}
+			rd.close();
+
+			conn.disconnect();
+
+			List<TimeSheetDaily> lsttimesheetinout = objectMapper.readValue(result,
+					new TypeReference<List<TimeSheetDaily>>() {
+					});
 			List<TimeSheetDaily> list_remove = new ArrayList<TimeSheetDaily>();
-			
-			//Lay ds nhan vien theo orgid 
+
+			// Lay ds nhan vien theo orgid
 			List<Personel> list_person = personService.getby_org(orgid_link, 1);
 			Map<Integer, Personel> map_person = new HashedMap<>();
-			for(Personel person : list_person) {
+			for (Personel person : list_person) {
 				int i_person_code = Integer.parseInt(person.getCode());
 				map_person.put(i_person_code, person);
 			}
-						
-			for(TimeSheetDaily daily : lsttimesheetinout) {
-				//Neu nhan su co trong danh sach nhan su cua don vi thi moi tinh, neu khong loai khoi danh sach
-				if(map_person.containsKey(Integer.parseInt(daily.getPersonnel_code()))) {
+
+			for (TimeSheetDaily daily : lsttimesheetinout) {
+				// Neu nhan su co trong danh sach nhan su cua don vi thi moi tinh, neu khong
+				// loai khoi danh sach
+				if (map_person.containsKey(Integer.parseInt(daily.getPersonnel_code()))) {
 					Personel person = map_person.get(Integer.parseInt(daily.getPersonnel_code()));
-					
+
 					daily.setFullname(person.getFullname());
 					daily.setPersonnel_code(person.getCode());
-					
-					if(grantid_link!= 0) {
-						if(!person.getOrgid_link().equals(grantid_link)) {
+
+					if (grantid_link != 0) {
+						if (!person.getOrgid_link().equals(grantid_link)) {
 							list_remove.add(daily);
 						}
 					}
-				}
-				else {
+				} else {
 					list_remove.add(daily);
 				}
 //				Personel person = personService.getPersonelBycode(daily.getPersonnel_code());
@@ -198,51 +204,125 @@ public class TimeSheetInOutAPI {
 			}
 			lsttimesheetinout.removeIf(c -> c.getFullname() == null);
 			lsttimesheetinout.removeAll(list_remove);
-			
-			response.data =lsttimesheetinout;
-			
+
+			response.data = lsttimesheetinout;
+
 			response.setRespcode(ResponseMessage.KEY_RC_SUCCESS);
 			response.setMessage(ResponseMessage.getMessage(ResponseMessage.KEY_RC_SUCCESS));
-			
+
 		} catch (Exception e) {
 			response.setRespcode(ResponseMessage.KEY_RC_EXCEPTION);
 			response.setMessage(e.getMessage());
 			System.out.println(e.getMessage());
 		}
 		return new ResponseEntity<getDailyResponse>(response, HttpStatus.OK);
-		
+
 	}
-	
+
 	@RequestMapping(value = "/calculate_daily", method = RequestMethod.POST)
 	public ResponseEntity<getDailyResponse> calculateDaily(@RequestBody getDailyRequest entity) {
 		getDailyResponse response = new getDailyResponse();
 		try {
 			long orgid_link = entity.orgid_link;
-			
-			String urlPost = AtributeFixValues.url_timesheet+"/timesheet/calculate_daily";
-			
-            
-            ObjectMapper objectMapper = new ObjectMapper();
-            ObjectNode appParNode = objectMapper.createObjectNode();
-            
-            //truyen param theo
-            appParNode.put("orgid_link", orgid_link);
-            String jsonReq = objectMapper.writeValueAsString(appParNode);
-            
-            HttpPost http = new HttpPost();
-            String result = http.getDataFromHttpPost(jsonReq, urlPost);
-            if("\"OK\"".equals(result))			
-            	response.setRespcode(ResponseMessage.KEY_RC_SUCCESS);
-            else 
-            	response.setRespcode(ResponseMessage.KEY_RC_EXCEPTION);
+
+			String urlPost = AtributeFixValues.url_timesheet + "/timesheet/calculate_daily";
+
+			ObjectMapper objectMapper = new ObjectMapper();
+			ObjectNode appParNode = objectMapper.createObjectNode();
+
+			// truyen param theo
+			appParNode.put("orgid_link", orgid_link);
+			String jsonReq = objectMapper.writeValueAsString(appParNode);
+
+			HttpPost http = new HttpPost();
+			String result = http.getDataFromHttpPost(jsonReq, urlPost);
+			if ("\"OK\"".equals(result))
+				response.setRespcode(ResponseMessage.KEY_RC_SUCCESS);
+			else
+				response.setRespcode(ResponseMessage.KEY_RC_EXCEPTION);
 			response.setMessage(ResponseMessage.getMessage(ResponseMessage.KEY_RC_SUCCESS));
-			
+
 		} catch (Exception e) {
 			response.setRespcode(ResponseMessage.KEY_RC_EXCEPTION);
 			response.setMessage(e.getMessage());
 		}
 		return new ResponseEntity<getDailyResponse>(response, HttpStatus.OK);
-		
+
 	}
-	
+
+	@RequestMapping(value = "/calculate_month", method = RequestMethod.POST)
+	public ResponseEntity<getDailyResponse> calculateMonth(@RequestBody getDailyRequest entity) {
+		getDailyResponse response = new getDailyResponse();
+		try {
+			long orgid_link = entity.orgid_link;
+			int month = entity.month;
+			int year = entity.year;
+
+			String urlPost = AtributeFixValues.url_timesheet + "/timesheet/calculate_month";
+
+			ObjectMapper objectMapper = new ObjectMapper();
+			ObjectNode appParNode = objectMapper.createObjectNode();
+
+			// truyen param theo
+			appParNode.put("month", month);
+			appParNode.put("year", year);
+			appParNode.put("orgid_link", orgid_link);
+			String jsonReq = objectMapper.writeValueAsString(appParNode);
+
+			HttpPost http = new HttpPost();
+			String result = http.getDataFromHttpPost(jsonReq, urlPost);
+			if ("\"OK\"".equals(result))
+				response.setRespcode(ResponseMessage.KEY_RC_SUCCESS);
+			else
+				response.setRespcode(ResponseMessage.KEY_RC_EXCEPTION);
+			response.setMessage(ResponseMessage.getMessage(ResponseMessage.KEY_RC_SUCCESS));
+
+		} catch (Exception e) {
+			response.setRespcode(ResponseMessage.KEY_RC_EXCEPTION);
+			response.setMessage(e.getMessage());
+		}
+		return new ResponseEntity<getDailyResponse>(response, HttpStatus.OK);
+
+	}
+
+	@RequestMapping(value = "/get_timesheet_month", method = RequestMethod.POST)
+	public ResponseEntity<GetTimeSheetMonthResponse> getTimeMonth(@RequestBody getDailyRequest entity) {
+		GetTimeSheetMonthResponse response = new GetTimeSheetMonthResponse();
+		try {
+			long orgid_link = entity.orgid_link;
+			int month = entity.month;
+			int year = entity.year;
+
+			String urlPost = AtributeFixValues.url_timesheet + "/timesheet/get_timesheet_month";
+
+			ObjectMapper objectMapper = new ObjectMapper();
+			ObjectNode appParNode = objectMapper.createObjectNode();
+
+			// truyen param theo
+			appParNode.put("month", month);
+			appParNode.put("year", year);
+			appParNode.put("orgid_link", orgid_link);
+			String jsonReq = objectMapper.writeValueAsString(appParNode);
+
+			HttpPost http = new HttpPost();
+			String result = http.getDataFromHttpPost(jsonReq, urlPost);
+			if ("\"ERR\"".equals(result))
+				response.setRespcode(ResponseMessage.KEY_RC_EXCEPTION);
+			else {
+				List<TimeSheetMonth> lsttimesheetmonth = objectMapper.readValue(result,
+						new TypeReference<List<TimeSheetMonth>>() {
+						});
+				response.data = lsttimesheetmonth;
+			}
+
+			response.setMessage(ResponseMessage.getMessage(ResponseMessage.KEY_RC_SUCCESS));
+
+		} catch (Exception e) {
+			response.setRespcode(ResponseMessage.KEY_RC_EXCEPTION);
+			response.setMessage(e.getMessage());
+		}
+		return new ResponseEntity<GetTimeSheetMonthResponse>(response, HttpStatus.OK);
+
+	}
+
 }
---
+++ src/main/java/vn/gpay/gsmart/core/timesheetinout/TimeSheetMonth.java
@@ -0,0 +1,257 @@
+package vn.gpay.gsmart.core.timesheetinout;
+
+public class TimeSheetMonth {
+	private long id;
+	private String personnel_code;
+	private float time_working;
+	private float time_plus;
+	private float time_over;
+	private float time_sunday;
+	private int lunch;
+	private float total;
+	private int nghi_om;
+	private int nghi_thaisan;
+	private int nghi_tainan;
+	private int nghi_le;
+	private int nghi_khongluong;
+	private int nghi_phep;
+	private int nghi_hoctap;
+	private int nghi_vannghe;
+	private int nghi_bu;
+	private int nghi_choviec;
+	private int nghi_congtac;
+	private int nghi_tudo;
+	private int nghi_viecrieng;
+	private int nghi_khongphep;
+	private int nghi_cachly;
+	private int month;
+	private int year;
+	private long orgid_link;
+	private String ngayvao_congty;
+	private String fullname;
+
+	public long getId() {
+		return id;
+	}
+
+	public String getPersonnel_code() {
+		return personnel_code;
+	}
+
+	public float getTime_working() {
+		return time_working;
+	}
+
+	public float getTime_plus() {
+		return time_plus;
+	}
+
+	public float getTime_over() {
+		return time_over;
+	}
+
+	public float getTime_sunday() {
+		return time_sunday;
+	}
+
+	public int getLunch() {
+		return lunch;
+	}
+
+	public float getTotal() {
+		return total;
+	}
+
+	public int getNghi_om() {
+		return nghi_om;
+	}
+
+	public int getNghi_thaisan() {
+		return nghi_thaisan;
+	}
+
+	public int getNghi_tainan() {
+		return nghi_tainan;
+	}
+
+	public int getNghi_le() {
+		return nghi_le;
+	}
+
+	public int getNghi_khongluong() {
+		return nghi_khongluong;
+	}
+
+	public int getNghi_phep() {
+		return nghi_phep;
+	}
+
+	public int getNghi_hoctap() {
+		return nghi_hoctap;
+	}
+
+	public int getNghi_vannghe() {
+		return nghi_vannghe;
+	}
+
+	public int getNghi_bu() {
+		return nghi_bu;
+	}
+
+	public int getNghi_choviec() {
+		return nghi_choviec;
+	}
+
+	public int getNghi_congtac() {
+		return nghi_congtac;
+	}
+
+	public int getNghi_tudo() {
+		return nghi_tudo;
+	}
+
+	public int getNghi_viecrieng() {
+		return nghi_viecrieng;
+	}
+
+	public int getNghi_khongphep() {
+		return nghi_khongphep;
+	}
+
+	public int getNghi_cachly() {
+		return nghi_cachly;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public long getOrgid_link() {
+		return orgid_link;
+	}
+
+	public String getNgayvao_congty() {
+		return ngayvao_congty;
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public void setPersonnel_code(String personnel_code) {
+		this.personnel_code = personnel_code;
+	}
+
+	public void setTime_working(float time_working) {
+		this.time_working = time_working;
+	}
+
+	public void setTime_plus(float time_plus) {
+		this.time_plus = time_plus;
+	}
+
+	public void setTime_over(float time_over) {
+		this.time_over = time_over;
+	}
+
+	public void setTime_sunday(float time_sunday) {
+		this.time_sunday = time_sunday;
+	}
+
+	public void setLunch(int lunch) {
+		this.lunch = lunch;
+	}
+
+	public void setTotal(float total) {
+		this.total = total;
+	}
+
+	public void setNghi_om(int nghi_om) {
+		this.nghi_om = nghi_om;
+	}
+
+	public void setNghi_thaisan(int nghi_thaisan) {
+		this.nghi_thaisan = nghi_thaisan;
+	}
+
+	public void setNghi_tainan(int nghi_tainan) {
+		this.nghi_tainan = nghi_tainan;
+	}
+
+	public void setNghi_le(int nghi_le) {
+		this.nghi_le = nghi_le;
+	}
+
+	public void setNghi_khongluong(int nghi_khongluong) {
+		this.nghi_khongluong = nghi_khongluong;
+	}
+
+	public void setNghi_phep(int nghi_phep) {
+		this.nghi_phep = nghi_phep;
+	}
+
+	public void setNghi_hoctap(int nghi_hoctap) {
+		this.nghi_hoctap = nghi_hoctap;
+	}
+
+	public void setNghi_vannghe(int nghi_vannghe) {
+		this.nghi_vannghe = nghi_vannghe;
+	}
+
+	public void setNghi_bu(int nghi_bu) {
+		this.nghi_bu = nghi_bu;
+	}
+
+	public void setNghi_choviec(int nghi_choviec) {
+		this.nghi_choviec = nghi_choviec;
+	}
+
+	public void setNghi_congtac(int nghi_congtac) {
+		this.nghi_congtac = nghi_congtac;
+	}
+
+	public void setNghi_tudo(int nghi_tudo) {
+		this.nghi_tudo = nghi_tudo;
+	}
+
+	public void setNghi_viecrieng(int nghi_viecrieng) {
+		this.nghi_viecrieng = nghi_viecrieng;
+	}
+
+	public void setNghi_khongphep(int nghi_khongphep) {
+		this.nghi_khongphep = nghi_khongphep;
+	}
+
+	public void setNghi_cachly(int nghi_cachly) {
+		this.nghi_cachly = nghi_cachly;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public void setOrgid_link(long orgid_link) {
+		this.orgid_link = orgid_link;
+	}
+
+	public void setNgayvao_congty(String ngayvao_congty) {
+		this.ngayvao_congty = ngayvao_congty;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+}
